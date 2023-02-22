b2f165312eba7443d6be53113ce11b2c4eccb8ef
MichalWilczek <mich.wilcz@gmail.com>
2023-02-22 19:32:28 +0100

    Add functions for querying income data and expenses from db.

--- src/server/transactions.php
+++ src/server/transactions.php
@@ -84,6 +84,104 @@ function getTransactionOptionIDAssignedToUser($dbConnect, $userID, $categoryType
     }
 }
 
+function getUserIncomeTransactions($dbConnect, $userID, $dateFrom, $dateTo) {
+
+    $tableData = getCategoryTypeData("incomeTables");
+    $userTable = $tableData['userTableName'];
+
+    try {
+        $query = $dbConnect->prepare(
+            "SELECT 
+                incomes_user.id,
+                incomes_user.amount,
+                incomes_user.date_of_income,
+                $userTable.name,
+                incomes_user.income_comment
+            FROM (SELECT * FROM incomes WHERE incomes.user_id = :userID) as incomes_user
+            INNER JOIN $userTable
+            ON incomes_user.income_category_assigned_to_user_id = $userTable.id
+            WHERE 
+                incomes_user.date_of_income >= :dateFrom
+                AND incomes_user.date_of_income <= :dateTo
+            "
+        );
+        $query->bindValue(":userID", $userID, PDO::PARAM_INT);
+        $query->bindValue(":dateFrom", $dateFrom, PDO::PARAM_STR);
+        $query->bindValue(":dateTo", $dateTo, PDO::PARAM_STR);
+        $query->execute();
+        $resultsRows = $query->fetchAll();
+        $results = [];
+        foreach($resultsRows as $row) {
+            array_push($results, 
+                [
+                    "id" => $row["id"],
+                    "amount" => $row["amount"],
+                    "issue_date" => $row["date_of_income"],
+                    "category" => $row["name"],
+                    "comment" => $row["income_comment"]
+                ]
+            );
+        }
+        return $results;
+    } catch (Exception $error) {
+        echo "Server error while querying income transactions from the server";
+        echo $error;
+    }
+}
+
+function getUserExpenseTransactions($dbConnect, $userID, $dateFrom, $dateTo) {
+
+    $expenseTables = getCategoryTypeData("expenseTables");
+    $paymentTables = getCategoryTypeData("paymentTables");
+    $userTableExpenses = $expenseTables['userTableName'];
+    $userTablePayments = $paymentTables['userTableName'];
+
+    try {
+        $query = $dbConnect->prepare(
+            "SELECT 
+                expenses_user.id,
+                expenses_user.amount,
+                expenses_user.date_of_expense,
+                $userTableExpenses.name as expense_category,
+                $userTablePayments.name as payment_method,
+                expenses_user.expense_comment
+            FROM (SELECT * FROM expenses WHERE expenses.user_id = :userID) as expenses_user
+            INNER JOIN $userTableExpenses
+            ON expenses_user.expense_category_assigned_to_user_id = $userTableExpenses.id
+            INNER JOIN $userTablePayments
+            ON expenses_user.payment_method_assigned_to_user_id = $userTablePayments.id
+            WHERE 
+                expenses_user.date_of_expense >= :dateFrom
+                AND expenses_user.date_of_expense <= :dateTo
+            "
+        );
+        $query->bindValue(":userID", $userID, PDO::PARAM_INT);
+        $query->bindValue(":dateFrom", $dateFrom, PDO::PARAM_STR);
+        $query->bindValue(":dateTo", $dateTo, PDO::PARAM_STR);
+        $query->execute();
+        $resultsRows = $query->fetchAll();
+        $results = [];
+        foreach($resultsRows as $row) {
+            array_push($results, 
+                [
+                    "id" => $row["id"],
+                    "amount" => $row["amount"],
+                    "issue_date" => $row["date_of_expense"],
+                    "category" => $row["expense_category"],
+                    "payment_method" => $row["payment_method"],
+                    "comment" => "expense_comment"
+                ]
+            );
+        }
+        return $results;
+    } catch (Exception $error) {
+        echo "Server error while querying expense transactions from the server";
+        echo $error;
+    }
+}
+
+
+
 // function modifyTransactionOptionsForUser($dbConnect, $userID, $categoryType, $oldCategoryName, $newCategoryName=null) {
     
 //     $tableData = getCategoryTypeData($categoryType);
@@ -127,4 +225,25 @@ function getTransactionOptionIDAssignedToUser($dbConnect, $userID, $categoryType
 //         echo "Server error while modifying the user password";
 //     }
 // }
-// ?>
+// 
+
+// session_start();
+// require_once("db.php");
+// // $apiResult = getUserIncomeTransactions(
+// //     connectToDB(),
+// //     60,
+// //     '2023-02-10',
+// //     '2023-02-17'
+// // );
+// $apiResult = getUserExpenseTransactions(
+//     connectToDB(),
+//     60,
+//     '2023-02-10',
+//     '2023-02-17'
+// );
+
+// header("Content-Type: application/json");
+// echo json_encode($apiResult);
+// exit();
+
+?>
--- src/server/utils.php
+++
@@ -1,9 +0,0 @@
-<?php
-function generateErrorMessage($session_error_key, $error_message) {
-	session_start();
-	if (isset($_POST[$session_error_key])) {
-		echo $error_message;
-		unset($_POST[$session_error_key]);
-	}
-}
-?>
